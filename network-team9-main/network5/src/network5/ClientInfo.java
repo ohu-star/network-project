99616b96d7012cf6365d720fa2a3f11c58056d32
ohu-star <audtj0703@naver.com>
2022-12-14 12:39:25 +0900

    network term project

--- network-team9-main/network5/src/network5/ClientInfo.java
+++
@@ -1,9 +0,0 @@
-package network5;
-
-public class ClientInfo {
-	public static String Logined_id;	
-	public static String Logined_pwd;
-	public static boolean isConnected;
-	public static String ip;
-	public static String socketNum;
-}
